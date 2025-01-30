c0479f01fd8c346f55064f2ae85769a7f6190a80
Tokard <58886109+TokardDev@users.noreply.github.com>
2025-01-30 12:04:04 +0100

    rework of displaying manager
    + change text function
    + change talking character

--- engine/main.gd
+++ engine/main.gd
@@ -1,18 +1,11 @@
-extends TextureRect
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$UI/textbox.update_text("Woa")
+	$UI/textbox.update_talking("Orma")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
-
-func update_text(text: String) -> void:
-	$MainText.text = text
-	
-func update_talking(character_name: String) -> void:
-	$Name.text = character_name
--- engine/name.gd
+++ engine/name.gd
@@ -5,7 +5,7 @@ func _ready():
 	get_viewport().connect("size_changed", Callable(self, "_on_viewport_resized"))
 
 func resize_text() -> void:
-	add_theme_font_size_override("normal_font_size", size.y*0.75)
+	add_theme_font_size_override("normal_font_size", int(size.y*0.75))
 
 func _on_viewport_resized():
 	resize_text()
