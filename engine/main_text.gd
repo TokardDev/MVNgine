4bc3011e9f2e6e095a0a26763ff77a3bdac6f5d5
Tokard <58886109+TokardDev@users.noreply.github.com>
2025-01-30 11:45:13 +0100

    added window flexibility

---
+++ engine/main_text.gd
@@ -0,0 +1,15 @@
+extends RichTextLabel
+
+func _ready():
+	resize_text()
+	get_viewport().connect("size_changed", Callable(self, "_on_viewport_resized"))
+	
+func resize_text() -> void:
+	var desired_lines = 4
+	var available_height = size.y
+	var font_size = (available_height/desired_lines)*0.7
+	add_theme_font_size_override("normal_font_size", font_size)
+	
+
+func _on_viewport_resized():
+	resize_text()
