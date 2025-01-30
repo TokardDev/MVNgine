0b2cd613f3339a66d26e24503bf14a9dbe8725b8
Tokard <58886109+TokardDev@users.noreply.github.com>
2025-01-30 09:40:30 +0100

    cleaning

--- engine/main.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-var characters = {}
-
-func _ready():
-	var story_manager = preload("res://engine/core/story_manager/story_manager.gd").new()
-	story_manager.start_story("res://game/story/test.mvn")
